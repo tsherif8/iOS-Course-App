41100c34435ada41e93079bed3c55915d40623ee
Tarek El Naggar <tiko8642@gmail.com>
2015-11-29 17:58:13 -0700

    Initial Commit

---
+++ PleaseWork1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PleaseWork1
+//
+//  Created by Tarek  El Naggar on 2015-11-29.
+//  Copyright © 2015 Tarek  El Naggar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
